28907bcf5f179c5c3b0cbd75dab11b2350e995b5
Layla Ramos <133053777+Layla-Ramos@users.noreply.github.com>
2024-06-25 17:50:13 -0300

    Update tabelas.sql
    Correção da fase 1 - adição de novas tabelas

--- tabelas.sql
+++ tabelas.sql
@@ -46,8 +46,6 @@ CREATE TABLE AlunosNaTurma (
   FOREIGN KEY (turma_id) REFERENCES Turma(turmaId)
 );
 
--- CORREÇÃO QUE SE FEZ NECESSÁRIA NA ELABORAÇÃO DA FASE 1 DE NOSSO PROJETO
-
 CREATE TABLE DisciplinaNaTurma (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   turma_id INT NOT NULL,
@@ -55,3 +53,46 @@ CREATE TABLE DisciplinaNaTurma (
   FOREIGN KEY (turma_id) REFERENCES Turma(turmaId),
   FOREIGN KEY (disciplinaId) REFERENCES Disciplina(disciplinaId)
 );
+
+-- CORREÇÃO QUE SE FEZ NECESSÁRIA NA ELABORAÇÃO DA FASE 1 DE NOSSO PROJETO --
+
+CREATE TABLE Disciplina (
+disciplinaId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL, 
+professor VARCHAR(40) NOT NULL, 
+descricao VARCHAR(255) NOT NULL, 
+cargaHoraria INT NOT NULL,
+nivel VARCHAR(20) NOT NULL, 
+cursoid INT NOT NULL, 
+FOREIGN KEY (cursoid) REFERENCES Cursos(cursoid) );
+
+CREATE TABLE AlunosNosCursos (
+ id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+aluno_cpf CHAR(11) NOT NULL, 
+cursoid INT NOT NULL,
+FOREIGN KEY (aluno_cpf) REFERENCES Alunos(cpf), 
+FOREIGN KEY (cursoid) REFERENCES Cursos(cursoid) );
+
+CREATE TABLE Professores (
+professorId INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+nome VARCHAR(40) NOT NULL, 
+email VARCHAR(50) NOT NULL,
+FOREIGN KEY (professorId) REFERENCES Professores(professorId) );
+
+CREATE TABLE Notas (
+notaId INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+aluno_cpf CHAR(11) NOT NULL, 
+disciplinaId INT NOT NULL, nota DECIMAL(4, 2) NOT NULL,
+FOREIGN KEY (aluno_cpf) REFERENCES Alunos(cpf), 
+FOREIGN KEY (disciplinaId) REFERENCES Disciplina(disciplinaId) );
+
+CREATE TABLE DisciplinasCursadas ( 
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+aluno_cpf CHAR(11) NOT NULL, 
+disciplinaId INT NOT NULL, 
+FOREIGN KEY (aluno_cpf) REFERENCES Alunos(cpf), 
+FOREIGN KEY (disciplinaId) REFERENCES Disciplina(disciplinaId) );
+
+
+
+
